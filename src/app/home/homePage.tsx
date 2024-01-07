ac9d5e64751a479fb2a466efebaf561c8833fe1d
Melik Bağrıyanık <melik.bagriyanik@esfagrup.com>
2024-01-07 21:15:40 +0300

    created homePage

---
+++ src/app/home/homePage.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import { Col, Row } from "react-bootstrap";
+import image_1 from "@/images/image_1.jpg";
+import "./homePage.css";
+export default function HomePage() {
+  return (
+    <>
+      <div className="responsive-row">
+        <div className="left-side">
+          {" "}
+          <Image
+            src={image_1}
+            className="responsive-image"
+            width={500}
+            alt=""
+          ></Image>
+        </div>
+
+        <div className="right-side text-container">
+          <div className="text-title">
+            Lorem ipsum dolor sit amet consectetur
+          </div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+          dolor veritatis incidunt ipsum! Quidem unde accusamus perferendis illo
+          a tenetur recusandae modi hic, repellendus pariatur dolor! Id aut
+          eaque quia.
+        </div>
+      </div>
+      <div className="responsive-row ">
+        <div className="left-side text-container">
+          <div className="text-title">
+            Lorem ipsum dolor sit amet consectetur
+          </div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+          dolor veritatis incidunt ipsum! Quidem unde accusamus perferendis illo
+          a tenetur recusandae modi hic, repellendus pariatur dolor! Id aut
+          eaque quia.
+        </div>
+
+        <div className="right-side">
+          {" "}
+          <Image
+            src={image_1}
+            className="responsive-image"
+            width={500}
+            alt=""
+          ></Image>
+        </div>
+      </div>
+    </>
+  );
+}
